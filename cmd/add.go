5178511b4de73f4022a98680b6b90ded065f9a96
Auxence-M <auxencemedja@gmail.com>
2025-03-29 23:57:50 -0400

    small improvement on ad command

--- cmd/add.go
+++ cmd/add.go
@@ -14,15 +14,20 @@ var addCmd = &cobra.Command{
 	Aliases: []string{"addition"},
 	Short:   "Add multiple numbers",
 	Long: `Carry out an addition operation on the numbers provided. 
-Adds all the numbers together and prints out the result. Only numerical values are allowed.
-Examples: 
-	zero add 20.5 30 50 70
-	zero add --file data.json`,
+Adds all the numbers together and prints out the result. Only numerical values are allowed.`,
+	Example: `zero add 20.5 30 50 70
+zero add --file data.json
+zero add -- 10 5 -20 20`,
 	Run: addRun,
 }
 
 func addRun(cmd *cobra.Command, args []string) {
 	result := 0.0
+
+	if file != "" && len(args) > 0 {
+		fmt.Println("You cannot provide a file and values at the same time.")
+		os.Exit(1)
+	}
 
 	if file != "" {
 		b, err := os.ReadFile(file)
@@ -54,6 +59,5 @@ func addRun(cmd *cobra.Command, args []string) {
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
 	addCmd.Flags().StringVar(&file, "file", "", "File containing numbers/data")
 }
